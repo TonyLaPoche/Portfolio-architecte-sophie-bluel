ff0c240e27c77786179ffcae3a426bee9c648e5c
tonylapoche <terrade.antoine.pro@gmail.com>
2024-01-18 07:38:17 +0100

    Refacto: Documentation Init() + import adresse

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -1,8 +1,16 @@
-import { getWorks } from "./services/api/apiService.js";
-import { filterWorks } from "./services/handlers/filterHandler.js";
-
+import { displayFilters } from "./services/displayModels/displayFilters.js";
+import { getWorks } from "./services/api/routes/getWorks.js";
+/**
+ * INITIALISATION
+ *
+ * Cette fonction initialise le javascript de l'application.
+ * Elle est appelée lorsque le DOM est chargé.
+ *
+ * Elle appelle displayFilters() pour afficher les filtres,
+ * puis getWorks() pour récupérer les travaux et les afficher.
+ */
 const init = async () => {
-  filterWorks();
+  await displayFilters();
   await getWorks();
 };
 

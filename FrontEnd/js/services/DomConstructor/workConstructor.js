4ade4f4dafca574e5f7090036176139c35e5c5a8
tonylapoche <terrade.antoine.pro@gmail.com>
2024-01-25 17:13:21 +0100

    docs: mise à jour de la documentation de la méthode constructWork

--- FrontEnd/js/services/DomConstructor/workConstructor.js
+++ FrontEnd/js/services/DomConstructor/workConstructor.js
@@ -1,7 +1,7 @@
 /**
  * Construit un élément DOM pour un travail.
  * @param {Object} work - Objet représentant un travail.
- * @returns Un élément DOM représentant le travail.
+ * @returns {HTMLDivElement} Un élément DOM représentant le travail.
  */
 export const constructWork = (work) => {
   const workDiv = document.createElement("div");

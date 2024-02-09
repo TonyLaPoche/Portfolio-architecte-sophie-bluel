b46a71abf3c5ea0f62f8c60907e99dc059e1b592
tonylapoche <terrade.antoine.pro@gmail.com>
2024-02-09 15:13:58 +0100

    Ajout de la fonction handleModify pour gérer le clic sur le bouton de modification

---
+++ FrontEnd/js/services/authentification/handler/modifyHandler.js
@@ -0,0 +1,6 @@
+export const handleModify = (button) => {
+  button.addEventListener("click", () => {
+    console.log("modify button clicked");
+    // TODO Trigger the modify action to open modal
+  });
+};

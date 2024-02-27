e01352673a3b70cd053cd3e9334ca8e53df19ac3
tonylapoche <terrade.antoine.pro@gmail.com>
2024-02-27 16:07:38 +0100

    Supprimer une œuvre en utilisant l'URL spécifique

--- FrontEnd/js/services/authentification/handler/deleteHandler.js
+++ FrontEnd/js/services/authentification/handler/deleteHandler.js
@@ -1,14 +1,14 @@
 import { callApi } from "../../api/utils/callApi.js";
 
 export const deleteWork = async (id) => {
+  const url = `http://localhost:5678/api/works/${id}`;
   const options = {
     method: "DELETE",
     headers: {
-      "Content-Type": "application/json",
       authorization: `Bearer ${sessionStorage.getItem("token")}`,
     },
   };
-  const data = await callApi(`http://localhost:5678/api/works/${id}`, options);
+  const data = await fetch(url, options);
 
   return data;
 };

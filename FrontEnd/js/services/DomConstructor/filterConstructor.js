cc6804c965253f6c912a02f844e83d8a0454b093
tonylapoche <terrade.antoine.pro@gmail.com>
2024-02-27 16:07:52 +0100

    Ajouter la construction d'un élément DOM pour une liste de filtres

--- FrontEnd/js/services/DomConstructor/filterConstructor.js
+++ FrontEnd/js/services/DomConstructor/filterConstructor.js
@@ -4,10 +4,45 @@
  * Donné reçu en paramètre de la fonction displayFilters.
  * @returns {HTMLLIElement} Un élément DOM représentant les filtres
  */
-export const constructFilter = (filter) => {
+const constructFilter = (filter) => {
   const filterDiv = document.createElement("li");
   filterDiv.classList.add("filter_item");
   filterDiv.setAttribute("data-category", filter.name);
   filterDiv.innerHTML = `${filter.name}`;
   return filterDiv;
+};
+
+/**
+ *
+ * @returns {HTMLLIElement} Un élément DOM représentant le filtre par défaut
+ * @description Cette fonction construit un élément DOM représentant le filtre par défaut
+ * qui correspond à la catégorie "Tous".
+ * @example
+ * const defaultFilter = constructDefaultFilter();
+ * console.log(defaultFilter); //  <li class="filter_item filter_item_active" data-category="all">Tous</li>
+ *
+ */
+const constructDefaultFilter = () => {
+  const defaultFilter = document.createElement("li");
+  defaultFilter.classList.add("filter_item");
+  defaultFilter.setAttribute("data-category", "all");
+  defaultFilter.classList.add("filter_item_active");
+  defaultFilter.innerHTML = "Tous";
+  return defaultFilter;
+};
+
+/**
+ * @description Construit un élément DOM pour une liste de filtres.
+ * @param {{name, id}} filters  - Objet représentant un filtre.
+ * @returns  {HTMLUListElement} Un élément DOM représentant la liste des filtres
+ */
+export const constructFilterList = (filters) => {
+  const filtersUnorderedList = document.createElement("ul");
+  filtersUnorderedList.setAttribute("id", "filters_list");
+  filtersUnorderedList.appendChild(constructDefaultFilter());
+  filters.forEach((filter) => {
+    const filterDiv = constructFilter(filter);
+    filtersUnorderedList.appendChild(filterDiv);
+  });
+  return filtersUnorderedList;
 };

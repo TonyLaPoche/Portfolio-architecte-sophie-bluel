70c6ab556b56ea5dedc48eb81a0f8f878772d4a1
tonylapoche <terrade.antoine.pro@gmail.com>
2024-03-01 10:23:39 +0100

    Mise à jour du gestionnaire de filtres

--- FrontEnd/js/services/handlers/filterHandler.js
+++ FrontEnd/js/services/handlers/filterHandler.js
@@ -1,18 +1,17 @@
 import { displayWorks } from "../displayModels/displayWorks.js";
 import { getWorksStatesByCategory } from "../statements/stateManagers.js";
 
+const updateDisplay = (filter) => {
+  const worksData = getWorksStatesByCategory(filter);
+  displayWorks(worksData);
+};
 /**
  * Initialise les écouteurs d'événements pour les filtres.
  * Cette fonction met en place les écouteurs d'événements sur chaque élément de filtre.
  * Elle appelle updateDisplay avec le filtre sélectionné lorsqu'un élément de filtre est cliqué.
  */
 export const filtersHandlers = () => {
-  const filterItems = document.querySelectorAll("#filters_list .filter_item");
-
-  const updateDisplay = async (filter) => {
-    const worksData = getWorksStatesByCategory(filter);
-    displayWorks(worksData);
-  };
+  const filterItems = document.querySelectorAll(".filter_item");
 
   filterItems.forEach((item) => {
     item.addEventListener("click", function () {

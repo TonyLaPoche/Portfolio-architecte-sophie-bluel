bfb1d2a14efc0ed21a1e62f23f9c9e8b2f4ea11a
tonylapoche <terrade.antoine.pro@gmail.com>
2024-03-12 17:21:48 +0100

    Mise à jour de l'URL de suppression des travaux

--- FrontEnd/js/services/api/routes/deleteHandler.js
+++ FrontEnd/js/services/api/routes/deleteHandler.js
@@ -1,7 +1,7 @@
 import { callApi } from "../utils/callApi.js";
 
 export const deleteWork = async (id) => {
-  const url = `http://localhost:5678/api/works/${id}`;
+  const url = `works/${id}`;
   const options = {
     method: "DELETE",
     headers: {

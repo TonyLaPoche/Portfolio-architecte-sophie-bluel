cd6102440fa9adecbd18e6854421dbc36fd9bc62
tonylapoche <terrade.antoine.pro@gmail.com>
2024-03-14 09:33:31 +0100

    Mise à jour de l'affichage des filtres sur la page d'accueil

--- FrontEnd/js/services/displayModels/displayFilters.js
+++ FrontEnd/js/services/displayModels/displayFilters.js
@@ -2,11 +2,9 @@ import { filtersHandlers } from "../handlers/filterHandler.js";
 import { constructFilterList } from "../DomConstructor/filterConstructor.js";
 
 /**
- * Affiche les filtres dans l'élément avec l'ID "filters".
- * Cette méthode récupère les catégories à partir de l'API, crée les éléments de filtre correspondants
- * et les ajoute à la liste des filtres.
- * Enfin, elle appelle la méthode filterWorks() pour appliquer les filtres aux éléments.
- * @returns {Promise<void>} Une promesse résolue une fois que les filtres sont affichés.
+ * @description Affiche les filtres sur la page d'accueil.
+ * @param {Promise<{ id: number; name: string;}[]>} filters - Les catégories de travaux à afficher.
+ * @returns Ajoute des ELements HTML à la balise ID "Filters" sont affichés via la méthode de constructionFilterList.
  */
 export const displayFilters = async (filters) => {
   const filtersCible = document.querySelector("#filters");

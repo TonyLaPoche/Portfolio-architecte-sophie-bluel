427fad0bb4ebfee160c9fb1f629c5ae436adf40b
tonylapoche <terrade.antoine.pro@gmail.com>
2024-03-14 12:27:10 +0100

    Ajouter un attribut aria-label pour le projet dans la balise figure

--- FrontEnd/js/services/DomConstructor/workConstructor.js
+++ FrontEnd/js/services/DomConstructor/workConstructor.js
@@ -8,7 +8,7 @@ export const constructWork = (work) => {
   workDiv.setAttribute("data-id", "works-" + work.id);
   workDiv.classList.add("work");
   workDiv.innerHTML = `
-                      <figure data-id="works-${work.id}">
+                      <figure data-id="works-${work.id}" aria-label="projet_${work.title}">
                           <img src="${work.imageUrl}" alt="${work.title}" loading="lazy">
                           <figcaption>${work.title}</figcaption>
                       </figure>

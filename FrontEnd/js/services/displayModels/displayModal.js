9e881c0c102a6a3e680671b0017ae56f162fe287
tonylapoche <terrade.antoine.pro@gmail.com>
2024-03-18 10:25:39 +0100

    Mise à jour de la JSdoc

--- FrontEnd/js/services/displayModels/displayModal.js
+++ FrontEnd/js/services/displayModels/displayModal.js
@@ -1,5 +1,10 @@
 import { modalConstructor } from "../DomConstructor/modalConstructor.js";
 
+/**
+ * Cette fonction permet d'afficher ou de cacher un modal.
+ * Si le modal existe déjà et que hasModal est faux, le modal est supprimé.
+ * @param {boolean} hasModal
+ */
 export const displayModal = (hasModal) => {
   const body = document.querySelector("body");
   const modal = document.getElementById("modal");

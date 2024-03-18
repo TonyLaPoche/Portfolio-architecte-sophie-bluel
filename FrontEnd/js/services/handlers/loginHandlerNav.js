fd68b87176650dcc1905383a39fb5713d67ca342
tonylapoche <terrade.antoine.pro@gmail.com>
2024-03-18 10:24:20 +0100

    Ajouter des commentaires pour la gestion de la connexion utilisateur

--- FrontEnd/js/services/handlers/loginHandlerNav.js
+++ FrontEnd/js/services/handlers/loginHandlerNav.js
@@ -3,6 +3,8 @@ import { displayModify } from "../displayModels/displayModify.js";
 
 /**
  * Cette fonction permet de gérer la connexion et la déconnexion de l'utilisateur.
+ * Elle intéragie avec le DOM pour afficher les boutons de connexion et de déconnexion.
+ * Les méthodes CheckLog() et displayModify() sont utilisées pour vérifier si l'utilisateur est connecté et pour afficher les boutons de modification.
  */
 export function loginHandlerNav() {
   const loginElementHtml = document.getElementById("login");

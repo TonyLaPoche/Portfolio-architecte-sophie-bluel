6795b97171d94f7178a58b9c5457df13329cd680
tonylapoche <terrade.antoine.pro@gmail.com>
2024-03-18 10:25:44 +0100

    Mise à jour de la JSdoc

--- FrontEnd/js/services/displayModels/displayModify.js
+++ FrontEnd/js/services/displayModels/displayModify.js
@@ -1,5 +1,10 @@
 import { modifyConstructor } from "../DomConstructor/modifyConstructor.js";
 
+/**
+ * Cette fonction permet d'afficher les boutons de modification.
+ * Elle utilise la fonction modifyConstructor() pour créer les éléments du DOM.
+ * Ces éléments sont ensuite ajoutés à la div comportant l'ID #options.
+ */
 export const displayModify = () => {
   const modifyCible = document.querySelector("#options");
   const { svgElement, buttonElement } = modifyConstructor();

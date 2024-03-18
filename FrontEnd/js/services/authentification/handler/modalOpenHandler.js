df227f0e3ac1c3b6c60251faf38d7e751f0bf571
tonylapoche <terrade.antoine.pro@gmail.com>
2024-03-18 10:25:32 +0100

    Mise à jour de la JSdoc

--- FrontEnd/js/services/authentification/handler/modalOpenHandler.js
+++ FrontEnd/js/services/authentification/handler/modalOpenHandler.js
@@ -1,5 +1,11 @@
 import { displayModal } from "../../displayModels/displayModal.js";
 
+/**
+ * Ajoute un écouteur d'événement sur le bouton passé en paramètre.
+ * Lorsque le bouton est cliqué, la fonction displayModal() est appelée.
+ * Si un element avec l'ID #modal existe déjà, la fonction ne fait rien.
+ * @param {HTMLButtonElement} button
+ */
 export const modalOpenHandler = (button) => {
   button.addEventListener("click", () => {
     if (document.getElementById("modal")) {

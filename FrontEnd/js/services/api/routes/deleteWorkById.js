8b6eac5454b5cb0e8afa654b8accd0459de13a8d
tonylapoche <terrade.antoine.pro@gmail.com>
2024-03-21 13:03:04 +0100

    Supprimer un travail par son identifiant

--- FrontEnd/js/services/api/routes/deleteWorkById.js
+++ FrontEnd/js/services/api/routes/deleteWorkById.js
@@ -1,5 +1,10 @@
 import { callApi } from "../utils/callApi.js";
 
+/**
+ * @description Supprime un travail par son identifiant.
+ * @param {number} id
+ * @returns {Promise} Une promesse contenant la réponse de la requête.
+ */
 export const deleteWorkById = async (id) => {
   const url = `works/${id}`;
   const options = {
